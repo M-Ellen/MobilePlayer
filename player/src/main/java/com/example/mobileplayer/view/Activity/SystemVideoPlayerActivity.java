514dc0164fab689efb3847c5ff8535cc44b34707
pengzhimao <pzm1993@hotmail.com>
2017-08-30 19:56:22 +0800

    add view

--- player/src/main/java/com/example/mobileplayer/view/Activity/SystemVideoPlayerActivity.java
+++ player/src/main/java/com/example/mobileplayer/view/Activity/SystemVideoPlayerActivity.java
@@ -2,7 +2,13 @@ package com.example.mobileplayer.view.Activity;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -11,7 +17,75 @@ import com.example.mobileplayer.presenter.activity.BaseActivityPresenter;
 import com.example.mobileplayer.presenter.activity.SystemVideoPlayerActivityPresenter;
 import com.example.mobileplayer.view.callback.SystemVideoPlayerActivityView;
 
-public class SystemVideoPlayerActivity extends BaseActivity implements SystemVideoPlayerActivityView {
+public class SystemVideoPlayerActivity extends BaseActivity implements SystemVideoPlayerActivityView, View.OnClickListener {
+
+
+    private LinearLayout llTop;
+    private TextView tvName;
+    private ImageView ivBattery;
+    private TextView tvSystemTime;
+    private Button btnVoice;
+    private SeekBar seekbarVoice;
+    private Button btnSwichPlayer;
+    private LinearLayout llBottom;
+    private TextView tvCurrentTime;
+    private SeekBar seekbarVideo;
+    private TextView tvDuration;
+    private Button btnExit;
+    private Button btnVideoPre;
+    private Button btnVideoStartPause;
+    private Button btnVideoNext;
+    private Button btnVideoSiwchScreen;
+
+    @Override
+    public void initView() {
+
+        llTop = (LinearLayout)findViewById( R.id.ll_top );
+        tvName = (TextView)findViewById( R.id.tv_name );
+        ivBattery = (ImageView)findViewById( R.id.iv_battery );
+        tvSystemTime = (TextView)findViewById( R.id.tv_system_time );
+        btnVoice = (Button)findViewById( R.id.btn_voice );
+        seekbarVoice = (SeekBar)findViewById( R.id.seekbar_voice );
+        btnSwichPlayer = (Button)findViewById( R.id.btn_swich_player );
+        llBottom = (LinearLayout)findViewById( R.id.ll_bottom );
+        tvCurrentTime = (TextView)findViewById( R.id.tv_current_time );
+        seekbarVideo = (SeekBar)findViewById( R.id.seekbar_video );
+        tvDuration = (TextView)findViewById( R.id.tv_duration );
+        btnExit = (Button)findViewById( R.id.btn_exit );
+        btnVideoPre = (Button)findViewById( R.id.btn_video_pre );
+        btnVideoStartPause = (Button)findViewById( R.id.btn_video_start_pause );
+        btnVideoNext = (Button)findViewById( R.id.btn_video_next );
+        btnVideoSiwchScreen = (Button)findViewById( R.id.btn_video_siwch_screen );
+        mVideoView = (VideoView)findViewById(R.id.video_view);
+
+        btnVoice.setOnClickListener(this);
+        btnSwichPlayer.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+        btnVideoPre.setOnClickListener(this);
+        btnVideoStartPause.setOnClickListener(this);
+        btnVideoNext.setOnClickListener(this);
+        btnVideoSiwchScreen.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if ( v == btnVoice ) {
+            // Handle clicks for btnVoice
+        } else if ( v == btnSwichPlayer ) {
+            // Handle clicks for btnSwichPlayer
+        } else if ( v == btnExit ) {
+            // Handle clicks for btnExit
+        } else if ( v == btnVideoPre ) {
+            // Handle clicks for btnVideoPre
+        } else if ( v == btnVideoStartPause ) {
+            // Handle clicks for btnVideoStartPause
+        } else if ( v == btnVideoNext ) {
+            // Handle clicks for btnVideoNext
+        } else if ( v == btnVideoSiwchScreen ) {
+            // Handle clicks for btnVideoSiwchScreen
+        }
+    }
+
 
     private VideoView mVideoView;
     private Uri mUri;
@@ -20,11 +94,6 @@ public class SystemVideoPlayerActivity extends BaseActivity implements SystemVid
     @Override
     protected BaseActivityPresenter getPresenter() {
         return mPresenter;
-    }
-
-    @Override
-    public void initView() {
-        mVideoView = (VideoView)findViewById(R.id.video_view);
     }
 
     @Override
